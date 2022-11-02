95350f8cc031d293f9c6b22c0c4fa8d3686ca13d
Квиз <kvizpliz@192.168.0.16>
2022-11-02 21:36:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 1
+        int a;
+        a = 5 * 3 + 3 / 2;
+        System.out.println(a);
+
+        //задание 2
+        int N = 3;
+        int K = 14;
+        System.out.println(K % N);
+
+    }
+}
+
+
---
+++ src/MySecondLesson.java
@@ -0,0 +1,16 @@
+public class MySecondLesson {
+    public static void main(String[] args) {
+        int a;
+        int b = 7;
+        b = 4;
+        System.out.println(b);
+        b = b + 1;
+        b += 1;
+        System.out.println(b);
+
+
+
+    }
+}
+
+
