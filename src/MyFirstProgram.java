afa8dbaf93c78d62eb9ed35c8ea888d6a6383035
Truwarr <117225216+Truwarr@users.noreply.github.com>
2022-11-06 11:57:45 +0300

    Delete src directory

--- src/MyFirstProgram.java
+++
@@ -1,7 +0,0 @@
-public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello world, ugin");
-        System.out.println("а это уже вторая строчка");
-        
-    }
-}
